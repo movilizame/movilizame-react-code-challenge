8700e10b51905b0c371145694aaaf607d36c4307
Gastón Gerber <gastongerber1@gmail.com>
2022-07-04 19:56:17 -0300

    Delete MoviesDispatchContext and pass all the values by MoviesContext.

--- src/components/contexts/MoviesContext.js
+++ src/components/contexts/MoviesContext.js
@@ -1,8 +1,6 @@
 const { createContext, useReducer } = require("react")
 
 export const MoviesContext = createContext(null);
-export const MoviesDispatchContext = createContext(null);
-
 
 function moviesReducer(state, action) {
     switch (action.type) {
@@ -28,10 +26,8 @@ const initialState = {
 export default function MoviesContextProvider ({ children }) {
     const [auth, dispatch] = useReducer(moviesReducer, initialState);
     return (
-        <MoviesContext.Provider value={auth}>
-            <MoviesDispatchContext.Provider value={dispatch}>
-                { children }
-            </MoviesDispatchContext.Provider>
-        </MoviesContext.Provider>
+      <MoviesContext.Provider value={{ movies: auth.movies, dispatch}}>
+        {children}
+      </MoviesContext.Provider>
     )
 }
