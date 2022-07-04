b052be802047b618f51620c71a239fa197aa34ad
Gastón Gerber <gastongerber1@gmail.com>
2022-07-04 19:12:24 -0300

    Add the JSX. Two inputs and one button

--- src/components/ui/SearchMovies.js
+++ src/components/ui/SearchMovies.js
@@ -1,3 +1,25 @@
 export default function SearchMovies () {
-    return <div></div>;
+
+    return (
+        <div>
+            <div>
+                <p>Título</p>
+                <input
+                    onChange={handleTitle} 
+                    type="search"
+                    placeholder="Ingrese un título"
+                />
+            </div>
+            <div>
+                <p>Año hasta</p>
+                <input
+                    onChange={handleYear} 
+                    type="search"
+                    placeholder="Ingrese un año"
+                />
+            </div>
+            <button onClick={handleSubmit}>Submit</button>
+
+        </div>
+    )
 }
