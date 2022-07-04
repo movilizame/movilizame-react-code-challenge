073b43b4bb55a6937f319dca93c34a8a906d9136
Gastón Gerber <gastongerber1@gmail.com>
2022-07-04 20:40:34 -0300

    Add conditional render to show the movies.

--- src/components/ui/MoviesList.js
+++ src/components/ui/MoviesList.js
@@ -1,6 +1,31 @@
-import React, { useContext } from "react";
-import { MoviesDispatchContext } from "../contexts/MoviesContext";
+import React from "react";
+import { useMoviesContext } from "components/contexts/MoviesContext";
 
 export default function MoviesList () {
-    return <div></div>;
+    // const { dispatch } = useMoviesContext();
+    const { movies } = useMoviesContext();
+    const imageRoute = 'https://image.tmdb.org/t/p/w500';
+
+    return (
+        <div>
+            {movies.length > 0 ? 
+                (
+                    <div>
+                        {movies.map(movie => (
+                            <article>
+                                <img src={imageRoute.concat(movie.poster_path)} alt="" width="70" height="70"/>
+                                <div>
+                                    <h2>{movie.original_title}</h2>
+                                </div>
+                            </article>
+                        ))}
+                    </div>
+                )
+                :
+                <div>
+                    <p>No hay nada para mostrar</p>
+                </div>
+            }
+        </div>
+    )
 }
