19aa9b698d42b6bde2b5124242c262f16f6063a2
Gastón Gerber <gastongerber1@gmail.com>
2022-07-04 21:40:34 -0300

    Create a NavBar and add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "./src/components/ui/MoviesList.js",
     "./src/components/ui/WatchList.js",
     "./src/components/ui/SearchMovies.js",
-    "./src/components/screens/Home.js"
+    "./src/components/screens/Home.js",
+    "./src/components/ui/NavBar.js"
   ],
   theme: {
     extend: {},
