bd54c9301393f19a626354cb083857167d05b4e6
Gastón Gerber <gastongerber1@gmail.com>
2022-07-04 21:38:42 -0300

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/ui/MoviesList.js",
+    "./src/components/ui/WatchList.js",
+    "./src/components/ui/SearchMovies.js",
+    "./src/components/screens/Home.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
