affa2def8e990579a9361e1b3d01e4d0a04c2227
Gastón Gerber <gastongerber1@gmail.com>
2022-07-04 21:23:10 -0300

    Add the conditional render, and use Map to show the list

--- src/components/ui/WatchList.js
+++ src/components/ui/WatchList.js
@@ -1,6 +1,28 @@
-import { useContext } from "react";
-import { MoviesContext } from "../contexts/MoviesContext";
+import { useMoviesContext } from "components/contexts/MoviesContext.js";
 
 export default function WatchList () {
-    return <div></div>;
+    const { list } = useMoviesContext();
+    const { dispatch } = useMoviesContext();
+
+    return (
+        <div>
+            <h1>Mi Lista</h1>
+            {list.length > 0 ? 
+                (
+                    <ul>
+                        {list.map(item => (
+                            <article>
+                                <div>
+                                    <h2>{item.original_title}</h2>
+                                    <button>X</button>
+                                </div>
+                                <hr />
+                            </article>
+                        ))}
+                    </ul>
+                )
+                : <p>No hay nada en la lista...</p>
+            }
+        </div>
+    )
 }
