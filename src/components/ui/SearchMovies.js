493147b7c1d3ed4affe0d909bd9c1c79c44baf43
Gastón Gerber <gastongerber1@gmail.com>
2022-07-04 21:50:02 -0300

    Add the validation and onKeyPress event to submit with an Enter key. Add input's style

--- src/components/ui/SearchMovies.js
+++ src/components/ui/SearchMovies.js
@@ -7,6 +7,8 @@ export default function SearchMovies() {
 
   const [title, setTitle] = useState('');
   const [year, setYear] = useState('');
+  const [validateTitle, setValidateTitle] = useState(true);
+  const [validateYear, setValidateYear] = useState(true);
 
   const handleTitle = (e) => {
     setTitle(e.target.value);
@@ -17,38 +19,82 @@ export default function SearchMovies() {
   }
 
   async function handleSubmit() {
-    const moviesLoad = await loadMovies(title);
-    const movies = moviesLoad.results;
-    dispatch({
-      type: 'clear'
-    })
-    movies.forEach(movie => {
-      const currentDate = new Date(movie.release_date).getFullYear();
-      if (currentDate <= year) {
+    if (title.length > 0) {
+      setValidateTitle(true);
+      if (year.length === 4) {
+        setValidateYear(true);
+        const moviesLoad = await loadMovies(title);
+        const movies = moviesLoad.results;
         dispatch({
-          type: 'setMovies',
-          movies: movie
+          type: 'clear'
         })
+        movies.forEach(movie => {
+          const currentDate = new Date(movie.release_date).getFullYear();
+          if (currentDate <= year) {
+            dispatch({
+              type: 'setMovies',
+              movies: movie
+            })
+          }
+        })
+      } else {
+        setValidateYear(false);
       }
-    })
+    } else {
+      if (year.length === 4) {
+        setValidateYear(true);
+        setValidateTitle(false);
+      } else {
+        setValidateTitle(false);
+        setValidateYear(false);
+      }
+    }
   }
+
+  const handleKeypress = (e) => {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+    if (keyCode === 13) {
+      handleSubmit();
+    }
+  }
+  
   return (
     <div>
       <div>
         <p>Título</p>
         <input
-          onChange={handleTitle}
-          type="search"
-          placeholder="Ingrese un título"
-        />
+            onKeyPress={handleKeypress}
+            onChange={handleTitle}
+            type="search"
+            style={{ borderColor: validateTitle ? "black" : "red" }}
+            className="flex-auto px-3 py-1.5 border rounded focus:text-gray-700 focus:border-blue-600 focus:outline-none"
+            placeholder="Ingrese un título"
+          />
+          <p
+            id="msgID"
+            aria-live="assertive"
+            style={{ visibility: validateTitle ? "hidden" : "visible" }}
+            className="text-xs text-red-500 mr-20">
+            Ingrese un título válido
+          </p>
       </div>
       <div>
         <p>Año hasta</p>
         <input
-          onChange={handleYear}
-          type="number"
-          placeholder="Ingrese un año"
-        />
+            onKeyPress={handleKeypress}
+            onChange={handleYear}
+            type="number"
+            style={{ borderColor: validateYear ? "black" : "red" }}
+            className="flex-auto px-3 py-1.5 border rounded focus:text-gray-700 focus:border-blue-600 focus:outline-none"
+            placeholder="Ingrese un año"
+          />
+          <p
+            id="msgID"
+            aria-live="assertive"
+            style={{ visibility: validateYear ? "hidden" : "visible" }}
+            className="text-xs text-red-500 mr-20">
+            Ingrese un año válido
+          </p>
       </div>
       <button onClick={handleSubmit}>Submit</button>
 
