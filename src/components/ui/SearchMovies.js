94c81c8dffccfe43b82d43f5b4d65501b93a7b6b
Gastón Gerber <gastongerber1@gmail.com>
2022-07-04 21:51:17 -0300

    SearchMovies style

--- src/components/ui/SearchMovies.js
+++ src/components/ui/SearchMovies.js
@@ -57,14 +57,17 @@ export default function SearchMovies() {
       handleSubmit();
     }
   }
-  
+
   return (
     <div>
-      <div>
-        <p>Título</p>
-        <input
+      <div className='flex flex-wrap items-end gap-3'>
+        <div className="text-center">
+          <p className='text-lg font-semibold text-slate-900'>
+            Título
+          </p>
+          <input
             onKeyPress={handleKeypress}
-            onChange={handleTitle}
+            onChange={handleChange}
             type="search"
             style={{ borderColor: validateTitle ? "black" : "red" }}
             className="flex-auto px-3 py-1.5 border rounded focus:text-gray-700 focus:border-blue-600 focus:outline-none"
@@ -77,12 +80,14 @@ export default function SearchMovies() {
             className="text-xs text-red-500 mr-20">
             Ingrese un título válido
           </p>
-      </div>
-      <div>
-        <p>Año hasta</p>
-        <input
+        </div>
+        <div className="text-center">
+          <p className='text-lg font-semibold text-slate-900'>
+            Año hasta
+          </p>
+          <input
             onKeyPress={handleKeypress}
-            onChange={handleYear}
+            onChange={handleNumber}
             type="number"
             style={{ borderColor: validateYear ? "black" : "red" }}
             className="flex-auto px-3 py-1.5 border rounded focus:text-gray-700 focus:border-blue-600 focus:outline-none"
@@ -95,9 +100,27 @@ export default function SearchMovies() {
             className="text-xs text-red-500 mr-20">
             Ingrese un año válido
           </p>
+        </div>
+        <button
+          onClick={handleSubmit}
+          className="mb-4 px-6 py-2.5 bg-blue-600 text-white rounded hover:bg-blue-700 active:bg-blue-800"
+          type="submit"
+        >
+          <svg
+            className="w-4"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="currentColor"
+              d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0
+                208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 
+                99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128
+                0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+            >
+            </path>
+          </svg>
+        </button>
       </div>
-      <button onClick={handleSubmit}>Submit</button>
-
     </div>
-  )
+  );
 }
