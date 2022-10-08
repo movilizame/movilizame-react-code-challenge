1c7ed25fc9038cd05821434f229ce5d91a129fc0
Gregorio Elias <gregorioelias@MacBook-Pro-de-Marta.local>
2022-10-08 20:42:20 -0300

    added inputs and button structures on searchmovies component with some style sheet

--- src/components/ui/SearchMovies.js
+++ src/components/ui/SearchMovies.js
@@ -1,3 +1,17 @@
-export default function SearchMovies () {
-    return <div></div>;
+export default function SearchMovies() {
+  return (
+    <div>
+      <form className=" flex flex-row items-end">
+        <div className="flex mt-100 mr-15 mx-1 ">
+          <p>Titulo</p>
+          <input type="search" placeholder="Ingrese titulo" />
+        </div>
+        <div>
+          <p>Año</p>
+          <input type="number" placeholder="Ingrese año" />
+        </div>
+        <button>Buscar</button>
+      </form>
+    </div>
+  );
 }
