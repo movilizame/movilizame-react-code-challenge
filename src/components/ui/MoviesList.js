c0747732b49642c55d59a0a9bcf76adde857984f
Gregorio Elias <gregorioelias@MacBook-Pro-de-Marta.local>
2022-10-09 20:22:58 -0300

    Added JSX structure on MovieList component

--- src/components/ui/MoviesList.js
+++ src/components/ui/MoviesList.js
@@ -2,11 +2,29 @@ import { useMoviesContext } from '../contexts/MoviesContext';
 
 export default function WatchList() {
   const { movies } = useMoviesContext();
+  const imgRoute = 'https://image.tmdb.org/t/p/w300';
 
   return (
     <div>
       {movies.map((movie) => (
-        <div key={movie.id}> {movie.original_title}</div>
+        <div key={movie.id}>
+          <img
+            src={`${imgRoute}${movie.poster_path}`}
+            alt={movie.original_title}
+            width="120"
+            height="120"
+          />
+          <div>
+            <h2>{movie.original_title}</h2>
+            <div>
+              <p>Año: {new Date(movie.release_date).getFullYear()}</p>
+            </div>
+            <div>
+              <p>Puntuación: {movie.vote_average}</p>
+              <p>Popularidad: {Math.round(movie.popularity)}</p>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
