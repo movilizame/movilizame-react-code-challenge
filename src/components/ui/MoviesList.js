d6e3a33309e82404a782383dad9c34230fefce14
Gregorio Elias <gregorioelias@MacBook-Pro-de-Marta.local>
2022-10-09 20:48:53 -0300

    added styles on MoviesList component

--- src/components/ui/MoviesList.js
+++ src/components/ui/MoviesList.js
@@ -23,38 +23,47 @@ export default function WatchList() {
   return (
     <div>
       {movies.map((movie) => (
-        <div key={movie.id}>
+        <div key={movie.id} className="flex items-start">
           <img
             src={`${imgRoute}${movie.poster_path}`}
             alt={movie.original_title}
             width="120"
             height="120"
+            className="m-3"
           />
-          <div>
-            <h2>{movie.original_title}</h2>
+          <div className="mt-1">
             <div>
-              <p>Año: {new Date(movie.release_date).getFullYear()}</p>
+              <h2 className="mt-1 font-sans text-xl">{movie.original_title}</h2>
+              <div className="mt-3">
+                <p className="font-sans">
+                  Año: {new Date(movie.release_date).getFullYear()}
+                </p>
+              </div>
+              <div>
+                <p>Puntuación: {movie.vote_average}</p>
+                <p>Popularidad: {Math.round(movie.popularity)}</p>
+              </div>
             </div>
-            <div>
-              <p>Puntuación: {movie.vote_average}</p>
-              <p>Popularidad: {Math.round(movie.popularity)}</p>
-            </div>
-          </div>
-          {list.some((item) => item.id === movie.id) ? (
-            <div>
-              <button onClick={() => deleteOnList(movie)}>
-                Quitar de mi lista
+            {list.some((item) => item.id === movie.id) ? (
+              <div>
+                <button
+                  onClick={() => deleteOnList(movie)}
+                  className="text-emerald-100 bg-red-600 text-gray-300 border-red-700 p-1 mt-8 rounded-md border border-solid font-normal cursor-pointer select-none align-bottom"
+                >
+                  Quitar de mi lista
+                </button>
+              </div>
+            ) : (
+              <button
+                onClick={() => {
+                  onSubmit(movie);
+                }}
+                className="text-emerald-100 bg-green-500 border-green-700 p-1 mt-8 rounded-md font-normal border border-solid cursor-pointer select-none align-bottom"
+              >
+                Agregar a mi lista
               </button>
-            </div>
-          ) : (
-            <button
-              onClick={() => {
-                onSubmit(movie);
-              }}
-            >
-              Agregar a mi lista
-            </button>
-          )}
+            )}
+          </div>
         </div>
       ))}
     </div>
