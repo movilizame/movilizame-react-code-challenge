8fd16690ce10158eacbc7434223c426e4b2dccbd
Gregorio Elias <gregorioelias@MacBook-Pro-de-Marta.local>
2022-10-09 20:45:19 -0300

    added flex on Homepage

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -8,12 +8,14 @@ export default function Home() {
       <div className="text-center text-6xl mb-4 bg-pink-50 p-2">
         <h1>Peliculas para mirar</h1>
       </div>
-      <div>
-        <SearchMovies />
-        <MoviesList />
-      </div>
-      <div>
-        <WatchList />
+      <div className="flex flex-wrap">
+        <div>
+          <SearchMovies />
+          <MoviesList />
+        </div>
+        <div className="flex flex-wrap ml-60 mb-4">
+          <WatchList />
+        </div>
       </div>
     </div>
   );
